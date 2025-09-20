832438ec10d724225b78d1a961ce91847098f1ef
Lou <m.lou.landy@gmail.com>
2025-09-20 13:08:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 app = Flask(__name__)
 CORS(app)
 
-model_name = "facebook/blenderbot-400M-distill"
+model_name = "microsoft/DialoGPT-small"
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
